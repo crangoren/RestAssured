f1d7b2885010abac24d71957910a9e0c557e341c
R.K.Suleimanov <crangoren@gmail.com>
2023-08-23 14:26:02 +0300

    test added

--- src/test/java/api/ReqresTest.java
+++ src/test/java/api/ReqresTest.java
@@ -1,9 +1,11 @@
 package api;
 
 import io.restassured.http.ContentType;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static api.Constants.MAIN_URL;
 import static io.restassured.RestAssured.given;
@@ -20,5 +22,16 @@ public class ReqresTest {
                 .log()
                 .all()
                 .extract().body().jsonPath().getList("data", UserData.class);
+
+        userData.forEach(u -> Assert.assertTrue(u.getAvatar().contains(u.getId().toString())));
+
+        Assert.assertTrue(userData.stream().allMatch(u -> u.getEmail().endsWith("reqres.in")));
+
+        List<String> avatars = userData.stream().map(UserData::getAvatar).collect(Collectors.toList());
+        List<String> id = userData.stream().map(u -> u.getId().toString()).collect(Collectors.toList());
+
+        for (int i = 0; i < avatars.size(); i++) {
+            Assert.assertTrue(avatars.get(i).contains(id.get(i)));
+        }
     }
 }
